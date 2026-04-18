9305b5595b4885428ef1d77f6868629570d8b378
vinhelysia <subi64.64pubg@gmail.com>
2026-04-18 16:49:50 +0700

    deleted some bs

--- godot-fps-cogito-master/Scripts/Tool/run_generate_codebase_map.gd
+++
@@ -1,9 +0,0 @@
-extends SceneTree
-
-
-func _init() -> void:
-	var generator_script = load("res://Scripts/Tool/generate_codebase_map.gd")
-	var generator = generator_script.new()
-	var result = generator.generate()
-	print(JSON.stringify(result))
-	quit()
